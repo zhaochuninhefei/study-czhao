5f5cdbab71e1168653dde834af429650cee10d2c
zhaochuninhefei <39891183+zhaochuninhefei@users.noreply.github.com>
2020-06-15 11:25:06 +0800

    Update TestCase04Optional.java

--- jdk11-test/src/test/java/java8/TestCase04Optional.java
+++ jdk11-test/src/test/java/java8/TestCase04Optional.java
@@ -12,6 +12,9 @@ public class TestCase04Optional {
         me.test01_simple("天生我材必有用");
         me.test01_simple(null);
 
+        me.test02_returnOptional("天生我材必有用");
+        me.test02_returnOptional(null);
+
         System.out.println("over");
     }
 
@@ -30,12 +33,14 @@ public class TestCase04Optional {
         }
 
         try {
+            // 这是一个不好的使用范例
             printLine(Optional.ofNullable(line));
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 
+    // 不推荐将参数类型设计为Optional，Optional适合用于返回值类型
     private void printLine(Optional<String> line) {
         try {
             System.out.println(line.get().trim());
@@ -51,4 +56,26 @@ public class TestCase04Optional {
         // 利用orElseThrow，当原来对象是null时，抛出自己定义的异常
         System.out.println(line.orElseThrow(() -> new RuntimeException("也可以抛出自己定义的异常！")));
     }
+
+    // 使用Optional的正确打开姿势
+    private void test02_returnOptional(String line) {
+        Optional<String> lineOpt = createLineOptional(line);
+
+        // 仅在原来对象非null时执行传入的lambda表达式
+        lineOpt.ifPresent(s -> System.out.println(s.trim()));
+        // 利用orElse，当原来对象是null时，使用orElse传入的默认值
+        System.out.println(lineOpt.orElse(""));
+        // 利用orElseGet，当原来对象是null时，使用orElseGet传入的lambda表达式
+        System.out.println(lineOpt.orElseGet(() -> "天生我材必有用，" + "千金散尽还复来。"));
+        // 利用orElseThrow，当原来对象是null时，抛出自己定义的异常
+        System.out.println(lineOpt.orElseThrow(() -> new RuntimeException("也可以抛出自己定义的异常！")));
+    }
+
+    private Optional<String> createLineOptional(String line) {
+        // 实际开发中，这里也许会有比较复杂的逻辑，用于返回一个对象，而该方法不保证返回对象不为null;
+        // 因此使用该方法的地方必须判断返回值是否为null。。。
+        // 但如果我们将返回值用Optional包起来，那么对于调用该方法的地方而言，非空判断就可以很优雅了。
+        return Optional.ofNullable(line);
+    }
+
 }
